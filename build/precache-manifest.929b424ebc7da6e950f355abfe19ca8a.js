81966f3925f4da890e92c7730f697f202ef9defb
Karol Marynicz <marynicz.karol@gmail.com>
2020-02-12 18:43:41 +0100

    build commit
    build commit to gh-pages

---
+++ build/precache-manifest.929b424ebc7da6e950f355abfe19ca8a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9696e14f9efb340b9c25a4f7790afcec",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "f169cbd401528ce7cfdf",
+    "url": "/rock-paper-scissors/static/css/main.91233a78.chunk.css"
+  },
+  {
+    "revision": "4e4e966abad02277abc6",
+    "url": "/rock-paper-scissors/static/js/2.a33758f7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rock-paper-scissors/static/js/2.a33758f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f169cbd401528ce7cfdf",
+    "url": "/rock-paper-scissors/static/js/main.8a3bf518.chunk.js"
+  },
+  {
+    "revision": "64e646bdb234669a874b",
+    "url": "/rock-paper-scissors/static/js/runtime-main.b78a4508.js"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/rock-paper-scissors/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/rock-paper-scissors/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/rock-paper-scissors/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "f19a40fbc5296effc230454fa1295890",
+    "url": "/rock-paper-scissors/static/media/image-rules-bonus.f19a40fb.svg"
+  },
+  {
+    "revision": "f4c973f743c8b75de9da23335e126dcd",
+    "url": "/rock-paper-scissors/static/media/logo-bonus.f4c973f7.svg"
+  }
+]);
